8cd84b998109f35b735bc3b54c72873400c7829f
mhdafnan2003 <mhdafnan2003@gmail.com>
2025-07-22 20:43:15 +0530

    final form

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -2,13 +2,13 @@ import SlideUp from "@utils/animations/slideUp"
 
 const About = () => {
     return (
-        <section className="funder-says-area">
+        <section className="funder-says-area" id="about">
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
                         <SlideUp className="founder-say-part text-center">
                             <h3>
-                                We’re <span>Zenorix</span> — a team of passionate Computer Science friends chasing a shared dream. 
+                                We’re <span>Zenorix</span> — a team of passionate Engineering Students chasing a shared dream. 
                                 From day one, we knew we were building more than just code or designs — we’re building a legacy. 
                                 Each team member is a powerhouse — design, development, branding, or UI/UX. 
                                 We’re not just good — we’re <em>at our prime</em> and hungry to prove it. 
